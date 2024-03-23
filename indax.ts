eb57bd6810594d7920cdc6cd33ef51614c6fff77
adnan <smg29030@gmail.com>
2024-03-23 03:15:46 -0700

    my firs calculator

---
+++ indax.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+
+let calculator = await inquirer.prompt([
+    {
+        message: " put your first value",
+        type:   "number",
+        name : "firstvalue"
+    },
+    {
+        message: " put your second value",
+        type:   "number",
+        name : "secondvalue"
+    },
+    {
+        message: " Select your operator for performing program",
+        type:   "list",
+        name : "operator",
+        choices : [ "addition" , "subtraction" , "multiplication" , "divison" ],
+    },
+])
+if ( calculator.operator === "addition") {
+console.log(calculator.firstvalue + calculator.secondvalue)
+}
+else if ( calculator.operator === "sbtraction") {
+    console.log(calculator.firstvalue - calculator.secondvalue)
+}
+else if ( calculator.operator === "multiplication") {
+        console.log(calculator.firstvalue * calculator.secondvalue)
+}
+else if ( calculator.operator === "division") {
+            console.log(calculator.firstvalue / calculator.secondvalue)
+}
+
+            
+        
